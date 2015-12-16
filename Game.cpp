a914a085480d42effd35fd9110ac47416a3b17ed
HammadSiddiqui <ronytop12@yahoo.com>
2015-12-16 09:30:04 +0500

    Background Setup - Next Step -> add Movable objects

--- Game.cpp
+++ Game.cpp
@@ -8,7 +8,7 @@
  * Constructor creates an empty window object of size 640x480 pixels
  * And then, it calls all the initialization of other elements.
  * */
-Game::Game() : mWindow(sf::VideoMode(640,480,32),"Frogger")  {
+Game::Game() : mWindow(sf::VideoMode(450,520,32),"Frogger")  {
     //Load Font
     if(!font.loadFromFile("assets/Abduction.ttf"))
     {
@@ -18,10 +18,38 @@ Game::Game() : mWindow(sf::VideoMode(640,480,32),"Frogger")  {
     sf::Color blueColor(0,0,55);
     mWindow.clear(blueColor);
     //initialize frog to its position
+    initWindow();
     initFrog();
     initScorecard();
+    mWindow.display();
+
 
 }
+
+
+void Game::initWindow() {
+    //Draw Top Grass
+    /*
+    sf::Texture grass;
+    grass.loadFromFile("assets/finalGrass.png");
+    sf::Sprite finalGrass;
+    finalGrass.setTexture(grass);
+    finalGrass.scale(2,2);
+    finalGrass.setPosition(1,50);
+    mWindow.draw(finalGrass);
+*/
+    //Draw pavements
+    sf::Texture path;
+    path.loadFromFile("assets/background.png");
+    sf::Sprite footPath;
+    footPath.setTexture(path);
+    footPath.scale(2,2);
+    footPath.setPosition(1,50);
+    mWindow.draw(footPath);
+
+
+}
+
 
 void Game::initFrog() {
     sf::Texture frog;
@@ -31,14 +59,14 @@ void Game::initFrog() {
 
     frogSprite.setTexture(frog);
     frogSprite.setScale(2.0,2.0);
-    frogSprite.setPosition(mWindow.getSize().x/2,mWindow.getSize().y -30); //Initial position of the frog
+    frogSprite.setPosition(mWindow.getSize().x/2,mWindow.getSize().y -70); //Initial position of the frog
     mWindow.draw(frogSprite);
 }
 
 void Game::initScorecard() {
     sf::Text scoreText("000",font,11);
     scoreText.setCharacterSize(20);
-    scoreText.setPosition(mWindow.getSize().x/2 - scoreText.getGlobalBounds().width/2, 30);
+    scoreText.setPosition(mWindow.getSize().x/2 - scoreText.getGlobalBounds().width/2, 10);
     mWindow.draw(scoreText);
 
 }
@@ -68,7 +96,7 @@ void Game::processEvents() {
 
 void Game::render() {
    // mWindow.clear();
-    mWindow.display();
+   // mWindow.display();
 }
 
 
